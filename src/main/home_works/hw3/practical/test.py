3d19dc3d33c6ba2e968be2df535aff42a9a1a822
KB-83 <kajal.baghestani@gmail.com>
2024-05-02 14:59:49 +0330

    res folder added

---
+++ src/main/home_works/hw3/practical/test.py
@@ -0,0 +1,28 @@
+# Define the class
+class Node:
+    def __init__(self, name, dOfU, dOfV):
+        self.name = name
+        self.dOfU = dOfU
+        self.dOfV = dOfV
+        neighbors = []
+
+# get input
+input = list
+
+class Graph:
+    def __init__(self,nodesList):
+        self.nodesList = nodesList
+        distances = []
+        for node in nodeList:
+            distances.append(node)
+    def dijekstra(start,end):
+        distances[start] = 0
+        while(unvisited.size >0){
+            relax = unvisited.extractmin;
+        for neighbor in relax.neighbors :
+            if (distances[neighbor] > distances[start] + w[neighbor start]){
+                distances[neighbor] = distances[start] + w[neighbor start]
+            }
+        }
+        
+
---
+++ src/main/home_works/hw3/practical/testt.py
@@ -0,0 +1,5 @@
+from test import Node
+node2 = Node("a",1,3)
+print(node2.name)
+print(node2.dOfU)
+print(node2.dOfV)
